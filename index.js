bca1a23a540d48bdb28e6daae32331b3ba862924
Thu2n <thuany.silva@pditabira.com>
2025-09-22 06:50:58 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
-// Criar uma função que calcula o tempo restante, e recebe dataFutura
-
-const calcularTempoRestante = (dataFutura) => {
-    const agora = new Date().getTime()
-    const diferencaDeDatas = Datafutura - agora;
-
-    const segundosDeUmMinuto = 60 * 1000;
-    const segundosDeUmaHora = 60 * segundosDeUmMinuto;
-    const segundosDoDia = 24 * segundosDeUmaHora;
-
-    const dias = Math.floor(diferenca / segundosDoDia);
-    const horas = Math.floor((diferencaDeDatas % segundosDoDia) / segundosDeUmaHora);
-    const minutos = Math.floor((diferencaDeDatas % segundosDeUmaHora) / segundosDeUmMinuto);
-    const segundos = Math.floor((diferencaDeDatas % segundosDeUmMinuto) / 1000)
-
-    return {
-        dias,
-        horas,
-        minutos,
-        segundos
-    }
-
-}
-const dataFutura = new Date('2024-11-30T12:00:00').getTime()
-//console.log(calcularTempoRestante(dataFutura))
-
-//Função que atualiza o temporizador
-const atualizarTemporizador = () => {
-    const dataFutura = new Date('2024-11-30T12:00:00')
-    const tempoRestante = calcularTempoRestante(dataFutura);
-
-    document.getElementById('dias').innerText = `Dias ${tempoRestante.dias}`;
-    document.getElementById('horas').innerText = `Horas ${tempoRestante.horas}`;
-    document.getElementById('minutos').innerText = `Minutos ${tempoRestante.minutos}`
-    document.getElementById('segundos').innerText = `Segundos ${tempoRestante.segundos}`
-
+const mudaTitulo = (novoTitulo) => {
+    const titulo = document.getElementsById('titulo-principal')
+    titulo.innerText = novoTitulo
 }
 
-const intervalo = setInterval(atualizarTemporizador, 1000);
+setTimeout(() => mudaTitulo('Oba! Alterei o titulo'), 2000)
 
-//atualizarTemporizador()
+const listaDeLi = document.getElementByTagName('li')
+
+const listaDeCores = ['#d60000', '#05f7ab']
+
+const listaDeParagrafos =  document.getElementByTagName('p')
+
+const botao = document.getElementByTagName('button')[0]
+
+setTimeout(() => {
+    botao.innerText ='Clique com  onovo texto'
+    botao.style.fontSize ='60px'
+}, 3000)
+
+for(let paragrafo of listaDeParagrafos) {
+    paragrafo.classList = 'paragrafo'
+}
+
+for(let li in listaDeLi) {
+    listaDeLi[li].style.color = listaDeCores[li]
+}
+
+
+
